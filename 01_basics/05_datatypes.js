3c39a5e1d9b91823b58541d3cd672bfc3dc0916b
ICONIC94 <apurvshukla944@gmail.com>
2025-10-06 17:21:11 +0000

    databases

---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all Js code as newer version 
+// alert(3+3) // we are using nodejs , not browser 
+let name=56
+console.log(3+3) 
+console.log("Apurv")
+//number =>> 2 TO power 53
+//bigint
+//string ==> ""
+//boolean
+//null
+//undefined =>
+//symbol =>> unique
+console.log(typeof "name") // gives the data type
+console.log(typeof null) //gives object 
+// console.log
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
 console.log("APURV");
+const varrr=23
+let name = "Apurv" 
+let accounstate;
+/* dont use var , use let*/
+console.log(varrr)
+console.log([varrr,name,accounstate])
