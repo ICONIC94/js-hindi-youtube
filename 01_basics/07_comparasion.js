f2f7adbc43092b0506e0b0d3dcba834f82b3cc85
ICONIC94 <apurvshukla944@gmail.com>
2025-10-07 16:51:46 +0000

    comparasion done

---
+++ 01_basics/07_comparasion.js
@@ -0,0 +1,13 @@
+// answer in boolean 
+// console.log(2>1)
+// console.log("2">1)
+
+console.log(null>0)       //Because  equality == and comparasion >=,>,<,<= work differently  
+
+console.log(null ==0)
+console.log(null>=0) //conmparasion coverts null into a number , treating it as a 0
+
+
+// returns false for undefined in all cases
+
+console.log("2"===2)
