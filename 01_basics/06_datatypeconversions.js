349ca719bedadaf2f2497f54694dd5563e072a02
ICONIC94 <apurvshukla944@gmail.com>
2025-10-07 13:58:04 +0000

    conversion done

---
+++ 01_basics/06_datatypeconversions.js
@@ -0,0 +1,23 @@
+let num="33"
+console.log(num)
+console.log(typeof num)
+let Int_num=Number(num)
+console.log(Int_num)
+console.log(typeof Int_num)
+
+let ass="33abc"
+let numass=Number(ass)
+console.log(numass) //NaN
+console.log(typeof numass) // number 
+
+let ooop="num"
+let numooop=Number(ass)
+console.log(ooop)
+console.log(numooop) //NaN
+console.log(typeof numooop) 
+
+let ans=1
+let booleanans=Boolean(ans)
+console.log(booleanans)
+
+
