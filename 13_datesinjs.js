3eca26b63992d751539d7bbe11a4a4dbfb7b4fcc
Apurv Shukla <apurvshukla944@gmail.com>
2025-10-22 16:30:15 +0530

    date done

---
+++ 13_datesinjs.js
@@ -0,0 +1,19 @@
+// Dates *********
+let mydates= new Date()
+console.log(mydates)
+console.log(mydates.toDateString())
+//  console.log(mydates.toLocaleString())
+//  console.log(typeof mydates)  /// object 
+
+ let createmydate= new Date(2023,0,23)
+ let createnewdate= new Date("12-07-2023")
+
+//  console.log(createmydate)
+//  console.log(createnewdate)
+//  console.log(createnewdate.toDateString())
+//  let presentdate= Date.now()
+//  console.log(presentdate)
+
+ console.log(mydates.getDay())
+ console.log(mydates.getDate())
+ console.log(mydates.getMonth() + 1)
