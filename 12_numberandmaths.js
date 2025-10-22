94a631f93d593092e8ba41528897815d565c48f9
Apurv Shukla <apurvshukla944@gmail.com>
2025-10-22 12:20:11 +0530

    done

---
+++ 12_numberandmaths.js
@@ -0,0 +1,12 @@
+ const balance = new Number(122000)
+ console.log(balance)
+ console.log(balance.toString())
+ console.log(typeof balance.toString()) 
+ console.log(balance.toString().length)
+
+
+
+
+ //******************. MATHS.  *********************/
+ console.log(Math.random())
+ console.log(Math.floor((Math.random()*10) + 1))
